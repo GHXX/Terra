a25509085a3bd978bb5fea5d5f368a5883865576
GHXX <GHXX@gmx.at>
2017-04-11 03:59:20 +0200

    added the universal logarithm

--- src/math/tmath.h
+++ src/math/tmath.h
@@ -16,6 +16,14 @@ static inline float TMathRadToDeg(float radian)
 	return (float)((double)radian * (180.0 / M_PI));
 }
 
+// Additional math related utilities
+
+static inline float TLOGbOFv(float base, float value)
+{
+	return (float)(log10f(value) / log10f(base));
+	//log_a(x) = log_10(x)/log_10(a)
+}
+
 #include "ttuple.h"
 #include "tquaternion.h"
 #include "tmatrix.h"
